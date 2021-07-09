03ec1dab6e4c628bc08d106a1939955775f60759
jxnprince <68714241+jxnprince@users.noreply.github.com>
2021-07-09 14:20:43 -0600

    More setup

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -31,15 +31,6 @@ function App() {
         <Route path='/login' exact={true}>
           <LoginForm />
         </Route>
-        <Route path='/sign-up' exact={true}>
-          <SignUpForm />
-        </Route>
-        <ProtectedRoute path='/users' exact={true} >
-          <UsersList/>
-        </ProtectedRoute>
-        <ProtectedRoute path='/users/:userId' exact={true} >
-          <User />
-        </ProtectedRoute>
         <ProtectedRoute path='/' exact={true} >
           <h1>My Home Page</h1>
         </ProtectedRoute>
